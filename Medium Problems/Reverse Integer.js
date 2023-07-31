0ee7713c7de43c3a9b130b4b59380fdc4aae3204
Nayan Bagale <nvbagale@gmail.com>
2023-07-31 20:22:03 +0530

    git push

--- Medium Problems/Reverse Integer.js
+++ Medium Problems/Reverse Integer.js
@@ -1,18 +1,16 @@
-//[-231, 231 - 1]
-
 var reverse = function (x) {
     if(x === 0) return x
     let str = x.toString();
     let newStr = ((str[0]) === '-' || (str[0]) ===  '+') ? str[0] : ""
 
     for(let i=str.length-1; 0 <= i; i--){
-        if(str[i] === '-' || str[i] === '+' || str[i] === '0') continue
+        if(str[i] === '-' || str[i] === '+') continue
         newStr += str[i];
     }
 
     let num = parseInt(newStr);
-    if(((-2)**31)){
-        if((2**31)-1){
+    if(((-2)**31) <= num){
+        if((2**31)-1 >= num){
             return num;
         }else{
             return 0
@@ -28,4 +26,7 @@ console.log(reverse(-123));
 console.log(reverse(120));
 console.log(reverse(12));
 console.log(reverse(0));
+console.log(reverse(5));
+console.log(reverse(502));
 console.log(reverse(901000));
+console.log(reverse(1534236469));
