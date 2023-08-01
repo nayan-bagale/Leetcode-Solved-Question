1ad6632f70d27b43ed261a527440987901e5e82c
Nayan Bagale <nvbagale@gmail.com>
2023-08-01 22:39:55 +0530

    Accepted

--- Easy Problems/Remove Duplicate from Sorted Array.js
+++ Easy Problems/Remove Duplicate from Sorted Array.js
@@ -1,20 +1,20 @@
-// TODO: Complete remove Duplicate Question
 var removeDuplicates = function (nums) {
-  let array = [nums[0]],
-    num = nums[0],
+  let num = nums[0],
     k = 0;
 
   for (let i = 1; i < nums.length; i++) {
     if (num !== nums[i]) {
-      num = nums[i];
-      k += 1;
-      array.push(nums[i]);
+        k += 1;
+        num = nums[i];
+        let temp = nums[k];
+        nums[k] = nums[i];
+        nums[i] = temp;
+
     }
+    if(nums[0] > nums[i]) break
   }
-  console.log(k)
-  return array;
+  return k+1;
 };
 
 console.log(removeDuplicates([1, 1, 2]));
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
-// console.log(removeDuplicates())
