dd7d9daba4e0f43a9b10f65d9b863b1c082c8fff
Nayan Bagale <nvbagale@gmail.com>
2023-08-03 19:54:09 +0530

    Accepted

--- Easy Problems/Valid Parentheses.js
+++ Easy Problems/Valid Parentheses.js
@@ -1,10 +1,29 @@
-// TODO: Complete the Valid Prentheses Question
 var isValid = function (s) {
+  let array = [];
+  for(let i=0; i<s.length; i++){
+    if(s[i] === '(' || s[i] === '[' || s[i] === '{' ) {
+      array.push(s[i])
+    }else{
+      const pop = array.pop();
+      if (
+        (s[i] === ")" && pop === "(") ||
+        (s[i] === "]" && pop === "[") ||
+        (s[i] === "}" && pop === "{")
+      ) {
+        continue
+      } else {
+        array.push(s[i]);
+      }
+    }
+  }
 
-  return;
+  console.log(array)
+
+  return array.length < 1;
 };
 
 console.log(isValid("()"));
 console.log(isValid("()[]{}"));
 console.log(isValid("(]"));
 console.log(isValid("([{}])"));
+console.log(isValid("(])"));
