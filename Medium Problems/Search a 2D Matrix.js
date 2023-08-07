66cd15552fa556d7fe7c1b2fb180f62c9b3caa74
Nayan Bagale <nvbagale@gmail.com>
2023-08-07 23:06:18 +0530

    git push

--- Medium Problems/Search a 2D Matrix.js
+++ Medium Problems/Search a 2D Matrix.js
@@ -1,10 +1,23 @@
 var searchMatrix = function(matrix, target) {
-    for(let i=0; i<matrix.length; i++){
-        for(let j=0; j<matrix[i].length; j++){
-            if( target === matrix[i][j]){
-                return true
-            }
+    // for(let i=0; i<matrix.length; i++){
+    //     for(let j=0; j<matrix[i].length; j++){
+    //         if( target === matrix[i][j]){
+    //             return true
+    //         }
+    //     }
+    // }
+    // return false;
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j=0; j < matrix[i].length; j++) {
+        if (matrix[i][j] === target) {
+          return true;
+        }else if(matrix[i][matrix[i].length - 1 ] < target){
+          break
         }
+
+        
+      }
     }
     return false;
 };
@@ -24,3 +37,5 @@ console.log(
     [23, 30, 34, 60],
   ],13)
 );
+
+console.log(searchMatrix([[1], [3]], 3));
