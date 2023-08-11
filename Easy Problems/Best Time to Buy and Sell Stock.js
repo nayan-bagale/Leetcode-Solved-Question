4a05eb844d57d26d0d1f2b1ded121ec3450a533e
Nayan Bagale <nvbagale@gmail.com>
2023-08-11 22:36:31 +0530

    Accepted

---
+++ Easy Problems/Best Time to Buy and Sell Stock.js
@@ -0,0 +1,24 @@
+// TODO: Complete the code
+
+function maxProfit(prices) {
+    let left = 0;
+    let right = 1;
+    let max_profit = 0;
+    while (right < prices.length) {
+      if (prices[left] < prices[right]) {
+        let current_profit = prices[right] - prices[left];
+        max_profit = Math.max(current_profit, max_profit);
+      } else {
+        left = right;
+      }
+      right++;
+    }
+
+    return max_profit;
+};
+
+
+console.log(maxProfit([7,1,5,3,6,4]))
+console.log(maxProfit([7,6,4,3,1]))
+console.log(maxProfit([1, 2]));
+console.log(maxProfit([2, 4, 1]));
